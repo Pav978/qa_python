35caa97e808530506cdf66c983c6f7abf304c08b
Pavel Tuinov <paharb-1986@yandex.com>
2024-05-19 11:20:10 +0300

    2222222222222222222

--- tests.py
+++ tests.py
@@ -17,16 +17,19 @@ class TestBooksCollector:
     def test_add_new_book(self, collector):
         collector.add_new_book("Буратино")
         assert "Буратино" in collector.get_books_genre()
+
 # тест №3 - установление жанра книги, если книга есть в books_genre ее жанр входи в список genre
     def test_set_book_genre(self, collector):
         collector.add_new_book("Чиполино")
         collector.set_book_genre("Чиполино", "Фантастика")
         assert collector.get_book_genre("Чиполино") == "Фантастика"
+
 # тест №4 - вывод жанра книги по ее названию
     def test_get_book_genre(self, collector):
         collector.add_new_book("Образец")
         collector.set_book_genre("Образец", "Фантастика")
         assert collector.get_book_genre("Образец") == "Фантастика"
+
 # тест №5 - вывод списка книг с опредленным жанром
     def test_get_books_with_specific_genre(self, collector):
         collector.add_new_book("Звездные войны")
@@ -35,6 +38,7 @@ class TestBooksCollector:
         collector.set_book_genre("Чужой", "Ужасы")
         assert collector.get_books_with_specific_genre("Фантастика") == ["Звездные войны"]
         assert collector.get_books_with_specific_genre("Ужасы") == ["Чужой"]
+
 # тест №6 - вывод текущего словаря books_genre
     def test_get_books_genre(self, collector):
         collector.add_new_book("Тайна 3 планеты")
@@ -42,6 +46,7 @@ class TestBooksCollector:
         collector.set_book_genre("Тайна 3 планеты", "Фантастика")
         collector.set_book_genre("Кошмар", "Ужасы")
         assert collector.get_books_genre() == {"Тайна 3 планеты": "Фантастика", "Кошмар": "Ужасы"}
+
 # тест №7 - возврат книг, которые подходят детям
     def test_get_books_for_children(self, collector):
         collector.add_new_book("Гостья из будущего")
@@ -49,17 +54,20 @@ class TestBooksCollector:
         collector.set_book_genre("Гостья из будущего", "Фантастика")
         collector.set_book_genre("Дети кукурузы", "Ужасы")
         assert collector.get_books_for_children() == ["Гостья из будущего"]
+
 # тест №8 - добавление книги в список избранных
     def test_add_book_in_favorites(self, collector):
         collector.add_new_book("Любимая книга")
         collector.add_book_in_favorites("Любимая книга")
         assert "Любимая книга" in collector.get_list_of_favorites_books()
+
 # тест №9 - удаление книги из списка избарнных
     def test_delete_book_from_favorites(self, collector):
         collector.add_new_book("Уже не любимая книга")
         collector.add_book_in_favorites("Уже не любимая книга")
         collector.delete_book_from_favorites("Уже не любимая книга")
         assert "Уже не любимая книга" not in collector.get_list_of_favorites_books()
+
 # тест №10 - вывод списка избранных книг
     def test_get_list_of_favorites_books(self, collector):
         collector.add_new_book("Хорошая книга")
@@ -68,17 +76,19 @@ class TestBooksCollector:
         collector.add_book_in_favorites("Лучшая книга")
         assert collector.get_list_of_favorites_books() == ["Хорошая книга", "Лучшая книга"]
 
-    # Негативные тесты
+# Негативные тесты
 # тест №11 - добавление пустого ввода в названии книги
     def test_add_new_book_with_empty_name(self, collector):
         collector.add_new_book("")
         assert "" not in collector.get_books_genre()
+
 # тест №12 - добавление книги в названии которого 41 символ
 # тест №13 - добавление книги в названии которого 40 символов
     @pytest.mark.parametrize('name', ['Ллллллллллллллллллллллллллллллллллллллллл', 'ьььььььььььььььььььььььььььььььььььььььь'])
     def test_add_new_book_length_name(self, collector, name):
         collector.add_new_book(name)
         assert collector.get_book_genre(name) is None
+
 # тест №14 - установление не существующего жанра для книги
     def test_set_book_genre_with_incorrect_genre(self, collector):
         collector.add_new_book("Книга 1")
